8f712764234e4b59a72db86b5102d00cc6756465
3levate <58492326+3levate@users.noreply.github.com>
2019-12-10 22:38:53 -0800

    Add files via upload

---
+++ Header.h
@@ -0,0 +1,17 @@
+#pragma once
+namespace house
+{
+	void openPackage();
+	void livingRoom();
+	void packActions();
+	void dineRoomHouse();
+	void action();
+}
+
+namespace global
+{
+	void fullInvPlayer();
+	void stats();
+	void help();
+	void actions();
+}
