f327bbbc1437aa4cbd7990e7644a85b9476f004a
3levate <58492326+3levate@users.noreply.github.com>
2020-01-09 21:46:03 -0800

    Update Hanger.cpp

--- Hanger.cpp
+++ Hanger.cpp
@@ -19,7 +19,7 @@ void Hanger::landingBay()
 	system("cls");
 	end();
 
-	print_slow(" Hanger Entry", 30);
+	print_slow(" Landing Bay", 30);
 	print_slow_15(" You walk down the corridor and come to the end. There is a doorway which you walk through into the shipping bay. It is massive.");
 	print_slow_15(" The ceiling is about 100 metres high and you see many workers preparing ships for takeoff and doing maintenance.");
 	print_slow_15(" There are four landing pads which each contain a ship. Each one has a guard protecting it. But you see some boxes that allow might allow you to sneak past then guard unoticed.");
@@ -27,11 +27,14 @@ void Hanger::landingBay()
 	print_slow_15(" The second ship has the most attack power out of the bunch but the least defense. It is black with golden highlights.");
 	print_slow_15(" The third ship has  the most defense but least attack power. It is green with black highlights.");
 	print_slow_15(" The fourth ship has the most health but least speed. It is silver and has black highlights.");
-	end();
+	SP_
+	system("pause");
+	SP_
 
 	do
 	{
 		getline(cin, input);
+		SP_
 		if (input == "get on first ship")
 		{
 			Hanger::firstShip();
@@ -77,32 +80,42 @@ void Hanger::fourthShip()
 void Hanger::ifStealthy()
 {
 	srand(time(NULL));
-	int yesno = ((rand() % 2) + 1);
+	int yesno = ((rand() % 3) + 1);
 
-	string ranOutOfTime = "You didn't attack the guard in time.";
-	string pressedButton = "You attack the guard.";
-	string options = "'1' to attack the guard";
+	string ranOutOfTime = " You didn't attack the guard in time.";
+	string pressedButton = " You attack the guard.";
+	string options = " '1' to attack the guard";
 	string timeDesc = " seconds until the guard reaches you.";
 
 	if (yesno == 1)
 	{
-		print("You snuck past the guard!");
+		print(" You snuck past the guard!");
 	}
 	else
 	{
-		print("You weren't sneaky enough.\n The guard heard a sound coming from the boxes and he starts walking over to them.");
-		//Global::timer(30000000, 4, ranOutOfTime, pressedButton, options, timeDesc, Hanger::gShipAttacks, );
+		print(" You weren't sneaky enough.\n The guard heard a sound coming from the boxes and he starts walking over to them.");
+		system("pause");
+		Global::timer(30000000, 4, ranOutOfTime, pressedButton, options, timeDesc, Hanger::gShipAttacks, Hanger::playerAttacks);
 	}
 }
 
 void Hanger::gShipAttacks()
 {
+	
+	int finalDamageGuard = GuardShip::easyReturnDamage() - Player::defense + Global::extraDamage();
+	Player::health = Player::health - finalDamageGuard;
 
+	end();
+	cout << " The guard walks around the boxes and shoots you in the back. It does " << finalDamageGuard << " damage." << endl;
+}
+
+void Hanger::playerAttacks()
+{
 	end();
 	srand(time(NULL));
 
 	int random = ((rand() % 5) + 1);
-	int finalDamage = Player::damage + gunBonus + random - GuardShip::guardEasyReturnDefense();
+	int finalDamage = Player::damage + gunBonus + random - GuardShip::easyReturnDefense();
 	GuardShip::minusVariables("health", finalDamage);
 
 	cout << "You attack the guard with your gun. It does " << finalDamage << " damage" << endl;
@@ -112,5 +125,4 @@ void Hanger::gShipAttacks()
 	cout << " health left" << endl;
 
 	getline(cin, input);
-
 }
