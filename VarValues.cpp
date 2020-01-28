051ffc4c02f67365ea90654e2d6fb177c6a7b551
3levate <58492326+3levate@users.noreply.github.com>
2020-01-28 15:19:05 -0800

    Update VarValues.cpp

--- VarValues.cpp
+++ VarValues.cpp
@@ -1,17 +1,6 @@
-#include <string>
-#include <iostream>
-#include <thread>
-#include <chrono>
-#include <Windows.h>
-#include <conio.h>
-#include <ctime>
-#include <time.h>
-#ifdef __cplusplus__
-#include <cstdlib>
-#else
-#include <stdlib.h>
-#endif
+#include "precompiled.h"
 #include "declarations.h"
+
 using namespace std;
 
 int GuardEasy::health = 11;
@@ -26,6 +15,7 @@ int Player::speed = 100;
 int Player::damage = 5;
 int Player::defense = 5;
 int Player::health = 20;
+int Player::xp = 0;
 
 int GuardImpossible::speed = 100000;
 int GuardImpossible::damage = 100000;
